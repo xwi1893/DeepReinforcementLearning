608e633957beb5af9c7e8d0f51c03df8744d4b26
xwi1893 <xuewei930227@yahoo.co.jp>
2020-05-11 15:00:47 +0900

    Update Reinforce.py

--- PolicyGradient/Reinforce.py
+++ PolicyGradient/Reinforce.py
@@ -1,1 +1,80 @@
 import numpy as np
+import tensorflow as tf
+
+class PolicyGradient:
+    def __init__(self, n_actions, n_features, learning_rate=0.01, reward_decay=0.95, output_graph=False):
+        self.n_actions = n_actions
+        self.n_features = n_features
+        self.lr = learning_rate
+        self.gamma = reward_decay
+
+        self.ep_obs, self.ep_as, self.ep_rs = [], [], []
+
+        self._build_net()
+
+        self.sess = tf.Session()
+
+        if output_graph:
+            tf.summary.FileWriter("logs/", self.sess.graph)
+
+        self.sess.run(tf.global_variables_initializer())
+
+    def _build_net(self):
+        with tf.name_scope("inputs"):
+            self.tf_obs = tf.placeholder(tf.float32, [None, self.n_features], name="observations")
+            self.tf_acts = tf.placeholder(tf.int32, [None, ], name="action_num")
+            self.tf_vt = tf.placeholder(tf.float32, [None, ], name="actions_value")
+
+        layer = tf.layers.dense(inputs=self.tf_obs, units=10, activation=tf.nn.tanh, kernel_initializer=tf.random_normal_initializer(mean=0, stddev=0.3),
+                                bias_initializer=tf.constant_initializer(0.1), name="fc1")
+
+        all_act = tf.layers.dense(inputs=layer, units=self.n_actions, kernel_initializer=tf.random_normal_initializer(mean=0, stddev=0.3),
+                                bias_initializer=tf.constant_initializer(0.1), name="fc2")
+
+        self.all_act_prob = tf.nn.softmax(all_act, name="act_prob")
+
+        with tf.name_scope("loss"):
+            # 最大化 总体 reward (log_p * R) 就是在最小化 -(log_p * R), 而 tf 的功能里只有最小化 loss
+            # neg_log_prob = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=all_act, labels=self.tf_acts)  # 所选 action 的概率 -log 值
+            # 下面的方式是一样的:
+            neg_log_prob = tf.reduce_sum(-tf.log(self.all_act_prob)*tf.one_hot(self.tf_acts, self.n_actions), axis=1)
+            loss = tf.reduce_mean(neg_log_prob * self.tf_vt)  # (vt = 本reward + 衰减的未来reward) 引导参数的梯度下降
+
+        with tf.name_scope("train"):
+            self.train_op = tf.train.AdamOptimizer(self.lr).minimize(loss)
+
+    def choose_action(self, observation):
+        prob_weights = self.sess.run(self.all_act_prob, feed_dict={self.tf_obs: observation[np.newaxis, :]})
+        action = np.random.choice(self.n_actions, p=prob_weights.ravel())
+        return action
+
+    def store_transition(self, s, a, r):
+        self.ep_obs.append(s)
+        self.ep_as.append(a)
+        self.ep_rs.append(r)
+
+    def learn(self):
+        discounted_ep_rs_norm = self._discount_and_norm_rewards()
+
+        self.sess.run(self.train_op, feed_dict = {
+            self.tf_obs: np.vstack(self.ep_obs),
+            self.tf_acts: np.array(self.ep_as),
+            self.tf_vt: discounted_ep_rs_norm
+        })
+
+        self.ep_obs, self.ep_as, self.ep_rs = [], [], []
+        return discounted_ep_rs_norm
+
+
+    def _discount_and_norm_rewards(self):
+        discounted_ep_rs = np.zeros_like(self.ep_rs)
+        running_add = 0
+        for t in reversed(range(len(self.ep_rs))):
+            running_add = running_add * self.gamma + self.ep_rs[t]
+            discounted_ep_rs[t] = running_add
+
+        # Normalize
+        discounted_ep_rs -= np.mean(discounted_ep_rs)
+        discounted_ep_rs /= np.std(discounted_ep_rs)
+        return discounted_ep_rs
+
